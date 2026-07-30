4d78ecbc4f082de19e9af9dc5bce35b7007c4e66
ZAHRAN88 <palastinearabia@gmail.com>
2026-07-30 20:26:46 +0300

    chore: bump all variant stock by +20

---
+++ src/Infrastructure/Data/Migrations/20260730172603_BumpAllStockBy20.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Oz.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class BumpAllStockBy20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE variant SET stock = stock + 20 WHERE is_archived = 0");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE variant SET stock = stock - 20 WHERE is_archived = 0");
+        }
+    }
+}
